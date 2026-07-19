a19716a39753d3dc5f1c8671ce00ce4bde8d769a
dolphin23-jp <h062.dolphin23@gmail.com>
2026-07-20 05:20:57 +0900

    T044: validate authored slice at runtime

--- src/content/runtime-content-validation.ts
+++ src/content/runtime-content-validation.ts
@@ -3,7 +3,11 @@ import {
   T039_PLAYER_CATALOG,
 } from '../demo/region-ui-demo'
 import { validateContentCatalog } from './content-validation'
+import { VERTICAL_SLICE_CONTENT_VALIDATION } from './vertical-slice-content'
 
 export const ACTIVE_CONTENT_VALIDATION = validateContentCatalog(T039_PLAYER_CATALOG, {
   genericSkillCosts: T039_GENERIC_SKILL_COSTS,
 })
+
+export const AUTHORED_VERTICAL_SLICE_CONTENT_VALIDATION =
+  VERTICAL_SLICE_CONTENT_VALIDATION
