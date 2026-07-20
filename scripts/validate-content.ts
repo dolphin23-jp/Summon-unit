3b99161e24fafee718f31b45cd3cde068512c5c8
dolphin23-jp <h062.dolphin23@gmail.com>
2026-07-20 20:12:18 +0900

    T053: validate skill FX master

--- scripts/validate-content.ts
+++ scripts/validate-content.ts
@@ -1,11 +1,12 @@
-import { UX_GUIDANCE_VALIDATION } from '../src/ux/ux-guidance'
 import { DISPLAY_MASTER_VALIDATION } from '../src/content/display-masters'
+import { VERTICAL_SLICE_SKILL_FX_VALIDATION } from '../src/content/skill-fx'
 import {
   ACTIVE_CONTENT_VALIDATION,
   AUTHORED_VERTICAL_SLICE_CONTENT_VALIDATION,
   AUTHORED_VERTICAL_SLICE_WORLD_VALIDATION,
   BALANCED_VERTICAL_SLICE_CONTENT_VALIDATION,
 } from '../src/content/runtime-content-validation'
+import { UX_GUIDANCE_VALIDATION } from '../src/ux/ux-guidance'
 
 console.log(
   JSON.stringify(
@@ -15,6 +16,7 @@ console.log(
       verticalSliceWorld: AUTHORED_VERTICAL_SLICE_WORLD_VALIDATION,
       verticalSliceBalanced: BALANCED_VERTICAL_SLICE_CONTENT_VALIDATION,
       displayMasters: DISPLAY_MASTER_VALIDATION,
+      skillFx: VERTICAL_SLICE_SKILL_FX_VALIDATION,
       uxGuidance: UX_GUIDANCE_VALIDATION,
     },
     null,
